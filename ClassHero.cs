6572d0cbfc510714695c367e7f3c54b14d16ed97
morescoutf <moresco@alunos.utfpr.edu.br>
2025-08-28 11:56:56 -0300

    Add files via upload
    codigo da segunda missao do classhero

---
+++ ClassHero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declaração das variáveis
+        string nomeProduto = "camiseta";
+        int quantidadeItens = 50;
+        double precoUnitario = 100.00;
+        bool produtoAtivo = true;
+
+        // Exibindo as informações usando concatenação
+        Console.WriteLine("Informações do Produto:");
+        Console.WriteLine("Nome: " + nomeProduto);
+        Console.WriteLine("Quantidade: " + quantidadeItens);
+        Console.WriteLine("Preço Unitário: R$" + precoUnitario);
+        Console.WriteLine("Ativo para venda: " + produtoAtivo);
+    }
+}
