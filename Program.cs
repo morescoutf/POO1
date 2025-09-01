1e5f2c46ab62163430d37cca522bdbf78025387e
morescoutf <moresco@alunos.utfpr.edu.br>
2025-08-31 21:12:59 -0300

    Add files via upload
    exercicio do guarda chuva

--- Program.cs
+++ Program.cs
@@ -2,19 +2,18 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        // Declaração das variáveis
-        string nomeProduto = "camiseta";
-        int quantidadeItens = 50;
-        double precoUnitario = 100.00;
-        bool produtoAtivo = true;
+        Console.Write("Está chovendo? (responda 'sim' ou 'não'): ");
+        string resposta = Console.ReadLine().Trim().ToLower();
 
-        // Exibindo as informações usando concatenação
-        Console.WriteLine("Informações do Produto:");
-        Console.WriteLine("Nome: " + nomeProduto);
-        Console.WriteLine("Quantidade: " + quantidadeItens);
-        Console.WriteLine("Preço Unitário: R$" + precoUnitario);
-        Console.WriteLine("Ativo para venda: " + produtoAtivo);
+        if (resposta == "sim")
+        {
+            Console.WriteLine("Você deve levar guarda-chuva.");
+        }
+        else
+        {
+            Console.WriteLine("Não precisa levar guarda-chuva.");
+        }
     }
 }
